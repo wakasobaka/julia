f31fa15fe113dd4546e6b9bcb9b7f0f8500bd036
waka <vitaliy.ilyin@gmail.com>
2021-09-19 21:31:03 +0300

    next step

--- task_0/robot.jl
+++ task_0/robot.jl
@@ -1,10 +1,22 @@
 using HorizonSideRobots
 
 r=Robot(10, 10, animate=true)
+
 move!(r, Ost)
-putmarker!(r)
-move!(r, Ost)
-move!(r, Ost)
+
 putmarker!(r)
 
+move!(r, Ost)
+
+move!(r, Ost)
+
+putmarker!(r)
+
+#show(r)
+
 #readline()
+
+#while not isborder(r,side)
+#    move!(r,side)
+#    num_steps+=1
+#end
